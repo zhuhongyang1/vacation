85db5bebae6ed3125d710488f84b7972774720f7
zhuhongyang1 <3394912213@qq.com>
2020-02-17 15:28:13 +0800

    2.17

---
+++ 朱洪洋实训二月考/pages/mb/mb.js
@@ -0,0 +1,66 @@
+// pages/mb/mb.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sj: ['看一场私人电影', '玩一次保龄球', '玩一次射箭', '练出几首自己的经典曲目', '好好','在大庭广众之下演讲一次']
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
